dbeece918c287888abb9cd97b716e589f2f1caf3
François Magimel <magimel.francois@gmail.com>
2018-08-21 20:00:49 +0200

    Bump the version to 0.9.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name="Cerealizer",
-    version="0.8.1",
+    version="0.9.0",
     license="Python licence",
     description="A secure pickle-like module",
     long_description="""A secure pickle-like module.
